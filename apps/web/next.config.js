31d49808d30e95687e3b6acc0b4a77b93aca8a10
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-17 21:00:45 +0200

    chore(apps/web): allowing external image request `next.config`

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['ui']
+  transpilePackages: ['ui'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
