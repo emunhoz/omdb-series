91b305828cc836d5850fdc794b8ac8533cf095cf
Eder Munhoz dos Santos <edermunhozsantos@gmail.com>
2023-06-17 20:57:04 +0200

    chore(apps/web): wrapping `react-query` layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from '@/providers/react-query/query-client-provider'
 import '../styles/global.css'
 
 export const metadata = {
@@ -12,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
